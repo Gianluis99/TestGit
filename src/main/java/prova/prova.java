0fe907981bf803ab9c3b49c71326aa9c77e33dcb
Gianluis99 <gianluigipanza31@gmail.com>
2021-10-25 13:20:35 +0200

    third commit

--- src/main/java/prova/prova.java
+++ src/main/java/prova/prova.java
@@ -7,4 +7,16 @@ public class prova {
 	private int tre;
 	private int qua;
 	
+	public prova(int uno, int due, int tre, int qua) {
+		super();
+		this.uno = uno;
+		this.due = due;
+		this.tre = tre;
+		this.qua = qua;
+	}
+	
+	
+	public void stampa() {
+		System.out.println(uno);
+	}
 }
